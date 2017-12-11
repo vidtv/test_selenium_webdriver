71b39170e657c44686c92d5dca329429e1947a41
vidtv <nikit1533@yandex.ru>
2017-12-12 00:17:21 +0300

    Methods on the result's page were added

--- src/main/java/ResultPage.java
+++ src/main/java/ResultPage.java
@@ -1,6 +1,5 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
 public class ResultPage {
     private static WebDriver driver;
@@ -9,22 +8,35 @@ public class ResultPage {
         this.driver = driver;
     }
 
-    @FindBy(xpath = "//input[@data-title='Завтра']/parent::label")
-    private WebElement tomorrowChoose;
+    private By dateChoose = By.xpath("//div[@class='dropdown__text js-dates__title']");
+    private By metroStationChoose = By.xpath("//div[@class='input-group__item'][2]//span[@class='tag__text']");
+    private By genreFirst = By.xpath("//div[@class='input__tags-inner js-filter_selected_list']/div[1]//span[@class='tag__text']");
+    private By genreSecond = By.xpath("//div[@class='input__tags-inner js-filter_selected_list']/div[1]//span[@class='tag__text']");
+    private By dayTime = By.xpath("//div[@class='dropdown__text js-range__title']");
+    private By seances2DOnly = By.xpath("//input[@name='is_2d']");
 
-    @FindBy(xpath = "//div[@data-id='68']//span[@class='tag__text']")
-    private WebElement kurskayaChoose;
+    public String getDate() {
+        return driver.findElement(dateChoose).getText();
+    }
 
-    @FindBy(xpath = "//div[@class='input__tags-inner js-filter_selected_list']/div[1]//span[@class='tag__text']")
-    private WebElement dramaChoose;
+    public String getMetroStation() {
+        return driver.findElement(metroStationChoose).getText();
+    }
 
-    @FindBy(xpath = "//div[@class='input__tags-inner js-filter_selected_list']/div[2]//span[@class='tag__text']")
-    private WebElement comedyChoose;
+    public String getFirstGenre() {
+        return driver.findElement(genreFirst).getText();
+    }
 
-    @FindBy(xpath = "//div[@class='dropdown__text js-range__title']")
-    private WebElement allDayChoose;
+    public String getSecondGenre() {
+        return driver.findElement(genreSecond).getText();
+    }
 
-    @FindBy(xpath = "//input[@name='is_2d']")
-    private WebElement seances2dChoose;
+    public String getTime() {
+        return driver.findElement(dayTime).getText();
+    }
+
+    public boolean is2DSeancesOnly() {
+        return driver.findElement(seances2DOnly).isSelected();
+    }
 
 }
